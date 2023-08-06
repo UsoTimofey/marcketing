2eeb1384817f7ef361d45cee21b222c86e17f9e3
Tima <usotimofej@gmail.com>
2023-08-06 20:11:34 +0300

    fourth commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 $(function(){
-    // var mixer = mixitup('.portfolio__content');
-    $('.slider-blog__inner').slick()
+    var mixer = mixitup('.portfolio__content');
+    $('.slider-blog__inner').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/arrow-left.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/arrow-right.png" alt=""></button>'
+    });
 });
